bfe29bad86ddeda21b590150ab1ab891d0193e1d
Loran Hendricks <lkh156@gmail.com>
2024-10-26 22:53:20 -0700

    Set up Svelte frontend for AD&D 2E

---
+++ frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    let message = "AD&D 2E Combat Simulator";
+</script>
+
+<main>
+    <h1>{message}</h1>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        margin-top: 50px;
+        font-family: 'Arial', sans-serif;
+    }
+</style>
